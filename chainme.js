f481f9550a0c5b87ca6a5a0f45db6a5ce7b6ed1d
özcan çetin <cetin445@gmail.com>
2022-05-16 19:04:08 +0300

    devam ediyor

---
+++ chainme.js
@@ -0,0 +1,33 @@
+// Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+// function add(num) {
+//   return num + 1;
+// }
+
+// function mult(num) {
+//   return num * 30;
+// }
+
+// chain(2, [add, mult]);
+// returns 90;
+
+//! çözüm
+let fs=[]
+function chain(input, fs){
+    function add(num){
+        return num+1
+    }
+    
+    function mult(num) {
+        return num * 30;
+    }
+
+    fs = [add, mult]
+   
+    return fs[1](fs[0](input))
+}
+
+console.log(chain(2, fs));
