dcd464447cf74ea326df2827d7257013b710c4be
özcan çetin <cetin445@gmail.com>
2022-05-22 06:59:42 +0300

    çözüldü

--- Replace_With_Alphabet_Position.js
+++ Replace_With_Alphabet_Position.js
@@ -10,44 +10,42 @@
 // Example
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
-let liste = ["abcdefghijklmnopqrstuvwxyz"];
-let list = liste.join().split("");
-let alphabet = [];
-for (let i = 0; i < list.length; i++) {
-  alphabet[list[i]] = String(i + 1);
-}
-console.log(alphabet["a"]);
-console.log(alphabet);
+
+
+// function alphabetPosition(text) {
+//   let liste = ["abcdefghijklmnopqrstuvwxyz"];
+//   let list = liste.join().split("");
+//   let alphabet = [];
+// for (let i = 0; i < list.length; i++) {
+//   alphabet[list[i]] = String(i + 1);
+// }
+//   let result ="";
+//   newtext=text.toLowerCase().split("")
+//   for (let i in newtext) {
+
+//     if(list.includes(newtext[i])){
+//         result += (alphabet[newtext[i]]) + " ";
+//     };
+//   }
+//   return result.slice(0, result.length-1)
+// }
+
+//! Ben uzun bir yöntem kullanarak harfleri ve sayıları barındıran bir sözlük oluşturmuştum. Ancak çok daha güzel yöntemler olduğunu öğrendim.:))
+
+console.log("A".charCodeAt());//65
+console.log("Z".charCodeAt());//90
+console.log("a".charCodeAt());//97
+console.log("z".charCodeAt());//122
 
 function alphabetPosition(text) {
-  let liste = ["abcdefghijklmnopqrstuvwxyz"];
-  let list = liste.join().split("");
-  let alphabet = [];
-  for (let i = 0; i < list.length; i++) {
-    alphabet[list[i]] = String(i + 1);
-  }
-  let newtext = text.toLowerCase()
-  let result = [];
-    for (let i = 0; i < newtext.split("").length; i++) {
-    if (newtext[i] in alphabet) {
-      result.push(alphabet[newtext[i]]);
-    } else {
-      result.push("?");
-    }
-  }
-  return result.join(" ");
+  let newtext = text.toLowerCase().split("").filter(test=>test.charCodeAt()>96 && test.charCodeAt() < 123 ).map(change=>change.charCodeAt()-96).join(" ")
+  return newtext
 }
-let z = "ELMA"
-console.log(z.toLowerCase());
-console.log(z.replace("E","e"));
-console.log(alphabet["A"]);
-console.log(alphabetPosition("The sunset sets at twelve o' clock."));
-console.log(alphabetPosition("a''a"));
-let a = ["a", "b"];
-console.log(a[0]);
-console.log("'" in alphabet);
-// console.log(a.includes("a"));
-// console.log(a.toLowerCase());
-// let b = ["a", "g"];
-// let c = b.filter((x) => a.includes(x));
-// console.log(c);
+
+let text = "Ali123"
+let newtext = 
+text.toLowerCase()//ali123
+.split("")//[ 'a', 'l', 'i', '1', '2', '3' ]
+.filter(test=>test.charCodeAt()>96 && test.charCodeAt() < 127 )//[ 'a', 'l', 'i' ]
+.map(change=>change.charCodeAt()-96)//[ 1, 12, 9 ]
+.join(" ")//1 12 9
