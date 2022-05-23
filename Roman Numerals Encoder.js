7d2ed53b40ce75b5c6ea6365d530348cb03b93ea
özcan çetin <cetin445@gmail.com>
2022-05-23 23:09:30 +0300

    devam ediyor

---
+++ Roman Numerals Encoder.js
@@ -0,0 +1,28 @@
+// 6 kyu Roman Numerals Encoder
+// Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+// Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+// Example:
+
+// solution(1000); // should return 'M'
+// Help:
+
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+// Remember that there can't be more than 3 identical symbols in a row.
+
+// More about roman numerals - http://en.wikipedia.org/wiki/Roman_numerals
+
+let romen={1:"I",5:"V",10:"X",50:"L",100:"C",500:"D",1000:"M"}
+let num = 55
+let kıyas = 0
+let liste = 0
+let result = String(num).length
+
