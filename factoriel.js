6402b20cee7ea1cda9b31b5c3d29784f6be3397c
özcan çetin <cetin445@gmail.com>
2022-06-19 14:47:23 +0300

    çözüldü

---
+++ factoriel.js
@@ -0,0 +1,17 @@
+function FirstFactorial(num) { 
+    let result = 1;
+    if(num == 0 || num == 1){
+        result = 1;
+    }
+    else if(num > 1){
+      for(let i = 1; i<=num; i++){
+        result *= i
+    }  
+    }
+    else{
+        result = ""
+    }
+    return result; 
+  }
+
+  console.log(FirstFactorial());
